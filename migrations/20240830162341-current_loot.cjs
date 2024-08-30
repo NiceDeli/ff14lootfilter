cbd5e730ddb8d7118fbc0514519b05f8e6cb45a4
Karlo <karloluis.arce@gmail.com>
2024-08-30 14:05:20 -0400

    current_loot fixing

---
+++ migrations/20240830162341-current_loot.cjs
@@ -0,0 +1,112 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    
+    await queryInterface.createTable('current_loot', {
+        id: {
+          type: Sequelize.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+          allowNull: false,
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+        },
+        static_mate_id: {
+          //here i need to add a foreign key with queryInterface.addConstraint
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        loot_table_id: {
+          //here i need to add a foreign key with queryInterface.addConstraint
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+      })
+      await queryInterface.addConstraint('current_loot', {
+        fields: ['static_mate_id'],
+        type: 'foreign key',
+        name: 'static_member',
+        references:{
+          table: 'static_mates', //this is refferencing the previous table  
+          field: "id" //this is refferencing the previous table
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      });
+      await queryInterface.addConstraint('current_loot', {
+        fields: ['loot_table_id'],
+        type: 'foreign key',
+        name: 'loot_table',
+        references:{
+          table: 'loot_table',
+          field: "id"
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      });
+      ;
+    },
+/*
+    async up (queryInterface, Sequelize) { 
+       await queryInterface.addConstraint('current_loot', {
+        fields: ['static_mate_id'],
+        type: 'foreign key',
+        name: 'static_member',
+        references:{
+          table: 'static_mate', //this is refferencing the previous table  
+          field: "id" //this is refferencing the previous table
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      });
+    },
+  
+    async up (queryInterface, Sequelize) { 
+      await queryInterface.addConstraint('current_loot', {
+        fields: ['loot_table_id'],
+        type: 'foreign key',
+        name: 'loot_table',
+        references:{
+          table: 'loot_table',
+          field: "id"
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      });
+    },
+*/
+     
+/*
+  async up (queryInterface, Sequelize) { 
+    queryInterface.addConstraint('current_loot', {
+      fields: ['id'],
+      type: 'primary key',
+      name: 'current_loot_id',
+      references:{
+        table: 'current_loot',
+        field: "id"
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    });
+  },
+  */
+  
+    async down (queryInterface, Sequelize) {
+      // Revert by dropping the static_mates table
+        await queryInterface.dropTable('current_loot');
+        }
+  };
+
+//the columns i made were the wrong type for the keys, the only way we could fix thing either modify or delete the table, 
+//
